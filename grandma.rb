736520251cefc7e3ce14c935e76267f3dbcdb4e4
Chad Montoya <chadmontoya21@yahoo.com>
2017-10-19 22:41:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string == string.uppercase
+  if string == string.upcase
     return "I LOVE YOU TOO PUMPKIN!"
   else
       "HUH?! SPEAK UP, SONNY!"
